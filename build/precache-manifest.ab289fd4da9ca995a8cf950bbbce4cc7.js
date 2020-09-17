8392ce0ccc752c67c402844e8e2f3b2924793a6e
sabirul-islam <67526212+sabirul-islam@users.noreply.github.com>
2020-09-18 03:21:56 +0600

    Add files via upload

---
+++ build/precache-manifest.ab289fd4da9ca995a8cf950bbbce4cc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63adc28920864a9007a3ed0ebd15df0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8eb93897556d9a05ec37",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0064f5787d2aa723af2f",
+    "url": "/static/css/main.cb50fc50.chunk.css"
+  },
+  {
+    "revision": "8eb93897556d9a05ec37",
+    "url": "/static/js/2.21acb83e.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/static/js/2.21acb83e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0064f5787d2aa723af2f",
+    "url": "/static/js/main.1b240dac.chunk.js"
+  },
+  {
+    "revision": "1aa57b96c533e52730d8",
+    "url": "/static/js/runtime-main.da6cc500.js"
+  }
+]);
